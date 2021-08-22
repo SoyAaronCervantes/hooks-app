f9a3292402b01a445f3c4927fddde55fae2c784f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-08-22 18:49:09 -0500

    Added postcss & tailwind

---
+++ src/HookApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HookApp.css';
+
+const HookApp = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  );
+};
+
+export default HookApp;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.tsx',
+    './src/**/*.ts'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
