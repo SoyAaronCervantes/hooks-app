91a956a269b341b7d3586692cfca98fb89331bda
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-08-27 00:39:04 -0500

    Added components folder

---
+++ src/components/01-useState.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CounterApp = () => {
+  return (
+    <>
+      <h1>Counter</h1>
+      <hr />
+      <button className={ "px-4" }>+1 </button>
+    </>
+  );
+};
+
+export default CounterApp;
